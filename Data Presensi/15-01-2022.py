e5a401536700bfcbbe6deddd0bc6a903b9277d48
riyan-eng <fbriyant11@gmail.com>
2022-01-15 09:30:14 +0700

    paham

---
+++ Data Presensi/15-01-2022.py
@@ -0,0 +1,10 @@
+siswa = [
+    'dahayu', 'calista', 'sahila', 'ata', 
+    'fahri', 'ilmi', 'haidar', 'yafi', 'zaky',
+    'bayu', 'ardan'
+]
+
+import random
+
+hasil = random.choice(siswa)
+print(hasil)
